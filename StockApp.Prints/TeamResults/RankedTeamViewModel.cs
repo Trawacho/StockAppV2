e536404b25f573ffaa53f6e98f9bd8dc8eb0d925
Trawacho <sankturm@gmail.com>
2022-09-24 10:25:55 +0200

    Erweitert um StokpunkteDifferenz

--- StockApp.Prints/TeamResults/RankedTeamViewModel.cs
+++ StockApp.Prints/TeamResults/RankedTeamViewModel.cs
@@ -4,10 +4,9 @@ namespace StockApp.Prints.TeamResults;
 
 public class RankedTeamViewModel
 {
-
     private readonly ITeam _team;
     private readonly bool _printNameOfPlayer;
-    public int Rank { get; }
+    public int Rank { get; init; }
     public string PlayerNames
     {
         get
@@ -32,6 +31,7 @@ public class RankedTeamViewModel
     public string TeamName => _team.TeamName.Trim();
     public string SpielPunkte => $"{_team.GetSpielPunkte().positiv }:{_team.GetSpielPunkte().negativ}";
     public string StockPunkte => $"{_team.GetStockPunkte().positiv}:{_team.GetStockPunkte().negativ}";
+    public string StockPunkteDifferenz => $"{_team.GetStockPunkteDifferenz()}";
     public string StockNote => _team.GetStockNote().ToString("F3");
 
     public RankedTeamViewModel(int rank, ITeam team, bool printNameOfPlayer)
