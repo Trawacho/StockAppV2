195b505179bf269c0a1a38e5ea3a00a20ae6f9bc
Trawacho <sankturm@gmail.com>
2022-09-25 15:16:41 +0200

    new Converter

---
+++ StockApp.UI/Converters/BooleanToVisibilityInvertedConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace StockApp.UI.Converters;
+
+internal class BooleanToVisibilityInvertedConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if(value is bool b)
+            return b ? Visibility.Collapsed : Visibility.Visible;
+        return new ArgumentException($"Value of type {value.GetType()} not allowed.");
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
