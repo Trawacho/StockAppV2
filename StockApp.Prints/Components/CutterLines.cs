820528dea1965ca0def8736188f96d84f98a3997
Trawacho <sankturm@gmail.com>
2023-01-15 15:00:17 +0100

    update

--- StockApp.Prints/Components/CutterLines.cs
+++ StockApp.Prints/Components/CutterLines.cs
@@ -3,39 +3,38 @@ using System.Windows;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
-namespace StockApp.Prints.Components
+namespace StockApp.Prints.Components;
+
+internal class CutterLines
 {
-    internal class CutterLines
+    internal static Line CutterLine()
     {
-        internal static Line CutterLine()
+        return new Line()
         {
-            return new Line()
-            {
-                X1 = 0,
-                X2 = 1,
-                Y1 = 0,
-                Y2 = 0,
-                Stretch = Stretch.Fill,
-                Stroke = Brushes.Black,
-                StrokeThickness = 1,
-                Margin = new Thickness(0, PixelConverter.CmToPx(0.75), 0, 0)
-            };
-        }
-
-        internal static Line CutterLineTop()
-        {
-            return new Line()
-            {
-                X1 = 0,
-                X2 = 1,
-                Y1 = 0,
-                Y2 = 0,
-                Stretch = Stretch.Fill,
-                Stroke = Brushes.Black,
-                StrokeThickness = 1,
-                Margin = new Thickness(0, 0, 0, PixelConverter.CmToPx(0.75))
-            };
-        }
-
+            X1 = 0,
+            X2 = 1,
+            Y1 = 0,
+            Y2 = 0,
+            Stretch = Stretch.Fill,
+            Stroke = Brushes.Black,
+            StrokeThickness = 1,
+            Margin = new Thickness(0, PixelConverter.CmToPx(0.75), 0, 0)
+        };
     }
+
+    internal static Line CutterLineTop()
+    {
+        return new Line()
+        {
+            X1 = 0,
+            X2 = 1,
+            Y1 = 0,
+            Y2 = 0,
+            Stretch = Stretch.Fill,
+            Stroke = Brushes.Black,
+            StrokeThickness = 1,
+            Margin = new Thickness(0, 0, 0, PixelConverter.CmToPx(0.75))
+        };
+    }
+
 }
