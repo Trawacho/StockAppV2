d7bc8e8db153c60bbc9d2c8635f9e111cff822f3
Trawacho <sankturm@gmail.com>
2024-03-31 16:51:11 +0200

    move to correct folder

--- StockApp.Prints/Template/TeamTemplateHelper.cs
+++ StockApp.Prints/Template/TeamTemplateHelper.cs
@@ -1,17 +1,14 @@
 ﻿using StockApp.Core.Turnier;
 using StockApp.Lib;
+using StockApp.Prints.Template;
+using System;
+using System.IO;
+using System.Printing;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Xps.Packaging;
-using System.Threading.Tasks;
-using System.IO;
-using System.Threading;
-using System;
-using System.Printing;
-using StockApp.Lib.Models;
-using System.Linq;
-using System.Diagnostics;
-using StockApp.Prints.Template;
 
 
 namespace StockApp.Prints.Teamresult;
@@ -27,11 +24,9 @@ public static class TeamTemplateFactory
     public static async Task<IDocumentPaginatorSource> Create(ITurnier turnier)
     {
         var token = new CancellationTokenSource();
-        Func<UIElement> reportFactory = () => new TeamTemplate(new TeamTemplateViewModel(turnier));
+        UIElement reportFactory() => new TeamTemplate(new TeamTemplateViewModel(turnier));
 
-        var paginator = new Paginator();
-        var printing = new Printing();
-        var helper = new TeamTemplateHelper(paginator, printing, turnier);
+        var helper = new TeamTemplateHelper();
         await helper.LoadReport(reportFactory, token.Token);
         return helper.GeneratedDocument;
     }
@@ -40,19 +35,19 @@ public static class TeamTemplateFactory
 
 internal class TeamTemplateHelper
 {
-    private readonly ITurnier _turnier;
+    private readonly IPaginator _paginator;
+    private readonly IPrinting _printing;
 
-    public TeamTemplateHelper(IPaginator paginator, IPrinting printing, ITurnier turnier)
+    public TeamTemplateHelper()
     {
-        _paginator = paginator;
-        _printing = printing;
-        _turnier = turnier;
+        _paginator = new Paginator();
+        _printing = new Printing();
     }
 
     /// <summary>
     /// XPS version of the FixedDocument. 
     /// </summary>
-    private XpsDocument xpsDocument;
+    private XpsDocument _xpsDocument;
 
     private IDocumentPaginatorSource generatedDocument;
     public IDocumentPaginatorSource GeneratedDocument
@@ -61,41 +56,21 @@ internal class TeamTemplateHelper
         set => generatedDocument = value;
 
     }
-    private readonly IPaginator _paginator;
-    private readonly IPrinting _printing;
-
-    private PrinterModel _selectedPrinter;
-    public PrinterModel SelectedPrinter
-    {
-        get => _selectedPrinter;
-        set => _selectedPrinter = value;
-    }
-
-    private PageSizeModel _selectedPageSize;
-    public PageSizeModel SelectedPageSize
-    {
-        get => _selectedPageSize;
-        set => _selectedPageSize = value;
-    }
-
-    private PageOrientationModel _selectedPageOrientation;
-    public PageOrientationModel SelectedPageOrientation
-    {
-        get => _selectedPageOrientation;
-        set => _selectedPageOrientation = value;
-    }
 
 
     internal async Task LoadReport(Func<UIElement> reportFactory, CancellationToken cancellationToken)
     {
-        string pdfPrinterName = "Microsoft Print to PDF";
-        SelectedPrinter = _printing.GetPrinters().Where(t => t.FullName == pdfPrinterName).FirstOrDefault();
-        SelectedPageSize = new PageSizeModel(new PageMediaSize(PageMediaSizeName.ISOA4));
-        SelectedPageOrientation = new PageOrientationModel(PageOrientation.Portrait);
+        var printTicket = new PrintTicket()
+        {
+            PageMediaSize = new PageMediaSize(PageMediaSizeName.ISOA4),
+            PageOrientation = PageOrientation.Portrait,
+            CopyCount = 1,
+            OutputColor = OutputColor.Color,
+            Duplexing = Duplexing.TwoSidedLongEdge,
+            PagesPerSheet = 1,
+        };
 
-        var printTicket = _printing.GetPrintTicket(SelectedPrinter.FullName, SelectedPageSize.PageMediaSize, SelectedPageOrientation.PageOrientation);
-
-        var printCapabilities = _printing.GetPrinterCapabilitiesForPrintTicket(printTicket, SelectedPrinter.FullName);
+        var printCapabilities = _printing.GetPrinterCapabilitiesForPrintTicket(printTicket, new System.Drawing.Printing.PrinterSettings().PrinterName);
 
         if (printCapabilities.OrientedPageMediaWidth.HasValue && printCapabilities.OrientedPageMediaHeight.HasValue)
         {
@@ -115,29 +90,29 @@ internal class TeamTemplateHelper
 
             // Delete old XPS file first
             CleanXpsDocumentResources();
-
-            xpsDocument = _printing.GetXpsDocumentFromFixedDocument(fixedDocument);
-            GeneratedDocument = xpsDocument.GetFixedDocumentSequence();
-
+            GeneratedDocument = fixedDocument;
+            return;
+            _xpsDocument = _printing.GetXpsDocumentFromFixedDocument(fixedDocument);
+            GeneratedDocument = _xpsDocument.GetFixedDocumentSequence();
         }
     }
 
 
     private void CleanXpsDocumentResources()
     {
-        if (xpsDocument != null)
+        if (_xpsDocument != null)
         {
             try
             {
-                xpsDocument.Close();
-                File.Delete(xpsDocument.Uri.AbsolutePath);
+                _xpsDocument.Close();
+                File.Delete(_xpsDocument.Uri.AbsolutePath);
             }
             catch
             {
             }
             finally
             {
-                xpsDocument = null;
+                _xpsDocument = null;
             }
         }
     }
