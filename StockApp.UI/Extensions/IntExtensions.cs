b0876d9e4852b4ad8705b770818eeb431580069a
Trawacho <sankturm@gmail.com>
2024-04-04 11:29:34 +0200

    Extension for Integer

---
+++ StockApp.UI/Extensions/IntExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace StockApp.UI.Extensions;
+
+internal static class IntExtensions
+{
+    internal static int InRange(this int value, int min, int max)
+    {
+        if (value < min) return min;
+        if (value > max) return max;
+        return value;
+    }
+}
