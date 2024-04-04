4f93f8351ec211bda850078a341351414765fdeb
Trawacho <sankturm@gmail.com>
2024-04-04 13:33:56 +0200

    move to lib

---
+++ StockApp.Lib/Extensions/IntExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace StockApp.Lib.Extensions;
+public static class IntExtensions
+{
+    public static int InRange(this int value, int min, int max)
+    {
+        if (value < min) return min;
+        if (value > max) return max;
+        return value;
+    }
+}
