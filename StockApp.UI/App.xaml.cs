d698ca826c2773680e720081dafad667f6f6c172
Trawacho <sankturm@gmail.com>
2024-04-18 16:10:09 +0200

    remove unused field

--- StockApp.UI/App.xaml.cs
+++ StockApp.UI/App.xaml.cs
@@ -28,7 +28,6 @@ namespace StockApp.UI
         private readonly IBroadcastService _broadCastService;
         private readonly MainViewModel _mainViewModel;
         private readonly MainWindow _mainWindow;
-        private readonly string _assemblyVersion;
 
         public App()
         {
@@ -37,7 +36,6 @@ namespace StockApp.UI
             _dialogStore.Register<LiveResultsZielViewModel, LiveResultZielView>();
 
             var _systemVersion = Assembly.GetExecutingAssembly().GetName().Version;
-            _assemblyVersion = _systemVersion.ToString();
             Software.Initialize(_systemVersion);
             Software.SanityCheckDirectories();
             PreferencesManager.Initialize();
