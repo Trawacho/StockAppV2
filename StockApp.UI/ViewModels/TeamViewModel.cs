acd8e4514e546d360983134bab5668c657800ac3
Trawacho <sankturm@gmail.com>
2024-07-27 12:19:43 +0200

    TeamViewModel angepasst

--- StockApp.UI/ViewModels/TeamViewModel.cs
+++ StockApp.UI/ViewModels/TeamViewModel.cs
@@ -9,6 +9,7 @@ using StockApp.UI.Settings;
 using StockApp.UI.Stores;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Windows.Input;
 
@@ -16,134 +17,183 @@ namespace StockApp.UI.ViewModels;
 
 public class TeamViewModel : ViewModelBase
 {
+	private readonly ITeam _team;
+	private readonly ITurnierStore _store;
+	private TeamPlayersViewModel _teamPlayersViewModel;
+	private readonly IEnumerable<string> _teamStatis;
+	private readonly IEnumerable<string> _teamInfos;
+	private readonly IEnumerable<IVerein> _vereine;
 
-    private readonly ITeam _team;
-    private readonly ITurnierStore _store;
-    private TeamPlayersViewModel _teamPlayersViewModel;
-    private readonly IEnumerable<string> _teamStatis;
-    private readonly IEnumerable<IVerein> _vereine;
+	public ITeam Team { get => _team; }
+	public string TeamName
+	{
+		get => _team.TeamName;
+		set
+		{
+			_team.TeamName = value;
+			RaisePropertyChanged();
+		}
+	}
 
-    public ITeam Team { get => _team; }
-    public string TeamName
-    {
-        get => _team.TeamName;
-        set
-        {
-            _team.TeamName = value;
-            RaisePropertyChanged();
-        }
-    }
+	public string StartNumber
+	{
+		get => _team.StartNumber.ToString(); set
+		{
+			if (int.TryParse(value, out int s))
+			{
+				_team.StartNumber = s;
+			}
+			RaisePropertyChanged();
+		}
+	}
 
-    public string StartNumber
-    {
-        get => _team.StartNumber.ToString(); set
-        {
-            if (int.TryParse(value, out int s))
-            {
-                _team.StartNumber = s;
-            }
-            RaisePropertyChanged();
-        }
-    }
+	public string Nation
+	{
+		get => _team.Nation;
+		set
+		{
+			_team.Nation = value;
+			RaisePropertyChanged();
+		}
+	}
+	public string Region
+	{
+		get => _team.Region;
+		set
+		{
+			_team.Region = value;
+			RaisePropertyChanged();
+		}
+	}
 
-    public string Nation
-    {
-        get => _team.Nation;
-        set
-        {
-            _team.Nation = value;
-            RaisePropertyChanged();
-        }
-    }
+	public string Bundesland
+	{
+		get => _team.Bundesland;
+		set
+		{
+			_team.Bundesland = value;
+			RaisePropertyChanged();
+		}
+	}
 
-    public IEnumerable<string> TeamStatis => _teamStatis;
+	public string Kreis
+	{
+		get => _team.Kreis;
+		set
+		{
+			_team.Kreis = value;
+			RaisePropertyChanged();
+		}
+	}
 
-    public string TeamStatus
-    {
-        get => _team.TeamStatus.Name();
-        set
-        {
-            _team.TeamStatus = TeamStatusExtension.FromName(value);
-            RaisePropertyChanged();
-        }
-    }
+	public IEnumerable<string> TeamStatis => _teamStatis;
+	public IEnumerable<string> TeamInfos => _teamInfos;
 
-    public int StrafSpielpunkte
-    {
-        get => _team.StrafSpielpunkte;
-        set
-        {
-            _team.StrafSpielpunkte = value;
-            RaisePropertyChanged();
-        }
-    }
+	public string TeamStatus
+	{
+		get => _team.TeamStatus.Name();
+		set
+		{
+			_team.TeamStatus = TeamStatusExtension.FromName(value);
+			RaisePropertyChanged();
+		}
+	}
 
-    public TeamPlayersViewModel TeamPlayersViewModel
-    {
-        get => _teamPlayersViewModel;
-        set => SetProperty(ref _teamPlayersViewModel, value);
-    }
+	public string TeamInfo
+	{
+		get => _team.TeamInfo.ToString();
+		set
+		{
+			_team.TeamInfo = Enum.TryParse(value, out TeamInfo teamInfo) 
+				? teamInfo 
+				: Core.Wettbewerb.Teambewerb.TeamInfo.Keine;
+			RaisePropertyChanged();
+		}
+	}
 
-    public TeamViewModel(ITeam team, ITurnierStore store)
-    {
-        _team = team;
-        _store = store;
-        TeamPlayersViewModel = new TeamPlayersViewModel(_team);
-        _teamStatis = Enum.GetValues(typeof(TeamStatus))
-                          .Cast<TeamStatus>()
-                          .Select(x => x.Name());
+	public int StrafSpielpunkte
+	{
+		get => _team.StrafSpielpunkte;
+		set
+		{
+			_team.StrafSpielpunkte = value;
+			RaisePropertyChanged();
+		}
+	}
 
-        _vereine = store.TemplateVereine;
-    }
+	public TeamPlayersViewModel TeamPlayersViewModel
+	{
+		get => _teamPlayersViewModel;
+		set => SetProperty(ref _teamPlayersViewModel, value);
+	}
 
-    protected override void Dispose(bool disposing)
-    {
-        if (!_disposed)
-        {
-            if (disposing)
-            {
-                TeamPlayersViewModel.Dispose();
-                _teamPlayersViewModel = null;
-            }
-            _disposed = true;
-        }
-    }
+	public TeamViewModel(ITeam team, ITurnierStore store)
+	{
+		_team = team;
+		_store = store;
+		TeamPlayersViewModel = new TeamPlayersViewModel(_team);
+		_teamStatis = Enum.GetValues(typeof(TeamStatus))
+						  .Cast<TeamStatus>()
+						  .Select(x => x.Name());
 
-    public IEnumerable<string> TemplateVereine => _vereine?.Select(x => x.Name);
+		_teamInfos = Enum.GetNames(typeof(TeamInfo));
 
-    public ICommand TeamSelectedEnterCommand => new RelayCommand(
-        (p) =>
-        {
-            var x = _vereine?.FirstOrDefault(v => v.Name == TeamName);
-            if (x != null)
-                Nation = x.Land + "/" + x.Region + "/" + x.Bundesland + "/" + x.Kreis;
-        },
-        (p) => true);
+		_vereine = store.TemplateVereine;
+	}
 
-    private ICommand _printReceiptCommand;
-    public ICommand PrintReceiptsCommand => _printReceiptCommand ??= new AsyncRelayCommand(
-        async (p) =>
-        {
-            var _printPreview = new PrintPreview(await ReceiptsFactory.Create(_store.Turnier, _team.StartNumber));
-            PreferencesManager.GeneralAppSettings.WindowPlaceManager.Register(_printPreview, "Receipt");
-            _printPreview.ShowDialog();
-        },
-        (p) => true);
+	protected override void Dispose(bool disposing)
+	{
+		if (!_disposed)
+		{
+			if (disposing)
+			{
+				TeamPlayersViewModel.Dispose();
+				_teamPlayersViewModel = null;
+			}
+			_disposed = true;
+		}
+	}
 
-    private ICommand _printWertungskarteCommand;
-    public ICommand PrintWertungskarteCommand => _printWertungskarteCommand ??= new AsyncRelayCommand(
-        async (p) =>
-        {
-            var _printPreview = new PrintPreview(
-                await ScoreCardsFactory.Create(_store.Turnier.ContainerTeamBewerbe.CurrentTeamBewerb
-                                             , _team.StartNumber
-                                             , PreferencesManager.TeamBewerbSettings.HasNamesOnScoreCard
-                                             , PreferencesManager.TeamBewerbSettings.HasSummarizedScoreCards     
-                                             , PreferencesManager.TeamBewerbSettings.HasOpponentOnScoreCard));
+	public IEnumerable<string> TemplateVereine => _vereine?.Select(x => x.Name);
+
+	public ICommand TeamSelectedEnterCommand => new RelayCommand(
+		(p) =>
+		{
+			var x = _vereine?.FirstOrDefault(v => v.Name == TeamName);
+			if (x != null)
+			{
+				Nation = x.Land;
+				Region = x.Region;
+				Bundesland = x.Bundesland;
+				Kreis = x.Kreis;
+			}
+		},
+		(p) => true);
+
+	private ICommand _printReceiptCommand;
+	public ICommand PrintReceiptsCommand => _printReceiptCommand ??= new AsyncRelayCommand(
+		async (p) =>
+		{
+			var _printPreview = new PrintPreview(await ReceiptsFactory.Create(_store.Turnier, _team.StartNumber));
+			PreferencesManager.GeneralAppSettings.WindowPlaceManager.Register(_printPreview, "Receipt");
+			_printPreview.ShowDialog();
+		},
+		(p) => true);
+
+	private ICommand _printWertungskarteCommand;
+	public ICommand PrintWertungskarteCommand => _printWertungskarteCommand ??= new AsyncRelayCommand(
+		async (p) =>
+		{
+			var _printPreview = new PrintPreview(
+				await ScoreCardsFactory.Create(_store.Turnier.ContainerTeamBewerbe.CurrentTeamBewerb
+											 , _team.StartNumber
+											 , PreferencesManager.TeamBewerbSettings.HasNamesOnScoreCard
+											 , PreferencesManager.TeamBewerbSettings.HasSummarizedScoreCards
+											 , PreferencesManager.TeamBewerbSettings.HasOpponentOnScoreCard));
 
 
-            PreferencesManager.GeneralAppSettings.WindowPlaceManager.Register(_printPreview, "WertungsKarte");
-            _printPreview.ShowDialog();
-        },
-        (p) => _store.Turnier.ContainerTeamBewerbe.CurrentTeamBewerb.GetCountOfGames() > 0);
+			PreferencesManager.GeneralAppSettings.WindowPlaceManager.Register(_printPreview, "WertungsKarte");
+			_printPreview.ShowDialog();
+		},
+		(p) => _store.Turnier.ContainerTeamBewerbe.CurrentTeamBewerb.GetCountOfGames() > 0);
 }
